e9e34d4c43795291eaf774c95798537f781e85a8
ryoma.nagata <ryoma.nagata0713.b@gmail.com>
2021-12-21 12:25:47 +0900

    regi source

---
+++ RegisterSource/README.md
@@ -0,0 +1,70 @@
+# Delete EntitiesByCollection
+
+## 使用方法
+
+### 1. ソースの取得
+
+git clone 後、移動します。
+
+```bash:bash
+
+cd purview-rest-tool\RegisterSource
+
+```
+
+### 2. 変数の準備
+
+- purviewAccountName:Purview名
+- collectionId:コレクションID
+- serverEndpoint:
+- sourceName:
+
+collectionIDの確認方法
+
+[Data Map]>[Collections]>[該当のコレクション]より、
+https://web.purview.azure.com/resource/<Purview名>/main/datasource/collections?collection=<コレクションID>
+の形式で確認可能です。
+
+![](.image/2021-12-14-11-51-10.png)
+
+### 3. 実行
+#### curl.sh
+
+トークンの確認方法
+
+- export token=
+ブラウザの開発者ツールを起動します。
+![](.image/2021-12-14-11-53-28.png)
+
+purview Studio 上で操作をすると、authorizationより確認可能です。
+
+![](.image/2021-12-14-11-54-53.png)
+
+
+変数をセットします。
+
+```bash:bash
+
+
+purviewAccountName=
+collectionId=
+serverEndpoint=
+sourceName=
+export token=
+
+```
+
+実行します。
+
+```bash:bash
+
+PURVIEWACCOUNTNAME=$purviewAccountName \
+COLLECTIONID=$collectionId \
+SERVERENDPOINT=$serverEndpoint \ 
+SOURCENAME=$sourceName \
+bash curl.sh
+
+```
+
+
+#### azrest.sh(未作成)
---
+++ RegisterSource/curl.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace # For debugging
+
+#############################################################
+# 引数
+
+purviewAccountName=$PURVIEWACCOUNTNAME
+collectionId=$COLLECTIONID
+serverEndpoint=$SERVERENDPOINT
+sourceName=$SOURCENAME
+
+##############################################################
+# 定数
+version='2018-12-01-preview'
+
+
+##############################################################
+# 変数
+
+baseUrl=https://${purviewAccountName}.scan.purview.azure.com/
+PutURL=${baseUrl}datasources/${sourceName}?api-version=${version}
+
+body=$(printf '{
+    "kind": "AzureSqlDataWarehouse",
+    "properties": {
+        "serverEndpoint":  "%s",
+        "collection": {
+            "type": "CollectionReference",
+            "referenceName": "%s"
+        }
+    }
+}'$serverEndpoint $collectionName  )
+
+
+##############################################################
+
+curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "$body" "$PutURL"
+
